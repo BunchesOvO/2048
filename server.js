dadb1f10488f255f0950774df78b2856e3ac41df
31601410 软工1602 杨以恒 <80412042@qq.com>
2019-06-19 05:25:14 +0800

    更新了登录功能

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var express = require('express'); 
-var app = express(); 
-var bodyParser = require('body-parser'); 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var flag=0;
 
 
 var urlencodedParser = bodyParser.urlencoded({
 	extended: false
 })
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 app.get('/index.html', function(req, res) {
-	res.sendFile(__dirname + "/" + form_index.html); 
+	res.sendFile(__dirname + "/" + form_index.html);
 })
 
-app.post('/process_post', urlencodedParser, function(req, res) { 
+app.post('/process_post', urlencodedParser, function(req, res) {
 
 	// 输出 JSON 格式
 	var response = {
@@ -22,10 +23,30 @@ app.post('/process_post', urlencodedParser, function(req, res) {
 	};
 
 	//加入數據庫
-	mysql_connec(req.body.names, req.body.passwords); 
+	adddata(req.body.names, req.body.passwords);
 	console.log(response);
-	res.send("1"); 
+	res.send("1");
 	res.end();
+
+})
+
+app.post('/login', urlencodedParser, function(req, res) {
+
+	// 输出 JSON 格式
+	var response = {
+		"names": req.body.names,
+		"passwords": req.body.passwords
+	};
+	
+	//查找數據庫
+	selectdata(req.body.names, req.body.passwords);
+	console.log(flag);
+	console.log(response);
+	res.header('Access-Control-Allow-Origin', '*');
+	if(flag){
+		res.send("1");
+		res.end();
+	}
 
 })
 
@@ -35,22 +56,20 @@ var server = app.listen(8081, function() { //监听
 	console.log("应用实例，访问地址为 http://%s:%s", host, port)
 })
 
-function mysql_connec(id, pwd) {
 
+//插入數據
+function adddata(id, pwd) {
 	var mysql = require('mysql');
 	var connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
 		password: '123456',
 		database: '2048',
-		port:'3306'
+		port: '3306'
 	})
 
 	connection.connect(); //连接
-
-
-	//插入數據
-	var addSql = "insert into user(id,pwd,score) values(?,?,0)"; 
+	var addSql = "insert into user(id,pwd,score) values(?,?,0)";
 	var addParmas = [id, pwd];
 	connection.query(addSql, addParmas, function(err, res) {
 		if (err) {
@@ -64,5 +83,43 @@ function mysql_connec(id, pwd) {
 
 	})
 
+}
+
+//查找數據
+function selectdata(id, pwd) {
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '123456',
+		database: '2048',
+		port: '3306'
+	})
+	connection.connect(); //连接
+	var selectSql = "select * from user where id=?";
+	var selectParms = [id];
+	connection.query(selectSql, selectParms, function(err, res) {
+		if (err) {
+			console.log('[select error]-', err.message);
+			return;
+		}
+		var temp = new String(res[0].pwd);
+		if (temp == pwd) {
+			flag=1;
+			console.log(flag);
+			console.log("登陆成功");
+			console.log("\n\n----------查詢語句-----------------------\n");
+			console.log(res);
+			console.log("\n----------查詢語句ENDENDNEDNED-----------");
+		} else {
+			flag=0;
+			console.log(flag);
+			console.log("密码错误");
+			
+			return;
+		}
+
+
+	})
 
 }
